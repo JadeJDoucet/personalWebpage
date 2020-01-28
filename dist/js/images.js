ba9f7d36429fc67ca59605b1d972c42e0f163874
jadejdoucet <jadejdoucet@gmail.com>
2020-01-28 15:14:15 -0600

    added image clicking to work images

--- dist/js/images.js
+++ dist/js/images.js
@@ -1,7 +1,8 @@
 // This will handle image src changing for each work item
 // Grab DOM items
 const Scannar = document.getElementById('scannar'); // Grab scannar image
-const Adopt = document.getElementById('#adopt'); // Grab Adopt A Pothole image
+const Adopt = document.getElementById('adopt'); // Grab Adopt A Pothole image
+const Readr = document.getElementById('readr');
 
 // Update our current image index
 const updateImageCounter = (index, array) => {
@@ -16,7 +17,8 @@ const updateImageCounter = (index, array) => {
 
 // Add click event listener
 Scannar.addEventListener('click', updateScannar);
-// Array of images
+
+// Array of image urls
 const scannarImages = [
   "img/projects/scannar-ss/scannar-login.png",
   "img/projects/scannar-ss/scannar-products.png",
@@ -26,9 +28,9 @@ const scannarImages = [
   "img/projects/scannar-ss/scannar-business-no-products.png",
   "img/projects/scannar-ss/scannar-business-create.png",
   "img/projects/scannar-ss/scannar-business-products.png"
-]; // Hold url for each scannar image
+];
 
- // Index for each image url
+// Index for current image
 let sIndex = 1;
 
 // Handle updating scannar
@@ -39,4 +41,41 @@ function updateScannar() {
 
 
 // -------------------------- Adopt A Pothole -------------------------- //
+Adopt.addEventListener('click', updateAdopt);
 
+// Array of image urls
+const adoptImages = [
+  "img/projects/adopt-a-pothole-ss/adopt-home.jpg",
+  "img/projects/adopt-a-pothole-ss/adopt-map.jpg",
+  "img/projects/adopt-a-pothole-ss/adopt-report.jpg"
+];
+
+// Index for current image
+let aIndex = 1;
+
+// Handle updating adopt
+function updateAdopt() {
+  Adopt.setAttribute("src", adoptImages[aIndex]); // Change curr image
+  aIndex = updateImageCounter(aIndex, adoptImages);
+};
+
+
+// -------------------------- Readr -------------------------- //
+Readr.addEventListener('click', updateReadr);
+
+// Array of image urls
+const readrImages = [
+  "img/projects/reader-ss/readr-login.png", // Starting image
+  "img/projects/reader-ss/readr-reader.png",
+  "img/projects/reader-ss/readr-saved.png",
+  "img/projects/reader-ss/readr-suggestion.png"
+];
+
+// Index for current image
+let rIndex = 1;
+
+// Handle updating adopt
+function updateReadr() {
+  Readr.setAttribute("src", readrImages[rIndex]); // Change curr image
+  rIndex = updateImageCounter(rIndex, readrImages);
+};
